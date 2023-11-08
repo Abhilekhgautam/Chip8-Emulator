593dfcfb0f9dcc691f8ba0563672615d945bdeed
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-11-08 22:25:05 +0545

    add driver code

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "../include/engine.h"
+#include "../include/chip8.h"
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char** argv){
+  if(argc != 2) {
+    std::cerr << "Invalid Arguments\n Usage: <program> <filepath>";
+    std::exit(1);
+  }	
+
+  Engine engine(800, 400);
+  Chip8 chip8;
+  chip8.loadMem(argv[1]);
+  engine.start(chip8.keyboard, chip8.video);
+
+  chip8.insCycle();
+}
